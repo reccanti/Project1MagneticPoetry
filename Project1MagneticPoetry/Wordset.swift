f2479cb22c6ade509bef8ef0ef4a80aee90226da
Ben Wilcox <reccanti@gmail.com>
2017-02-28 14:48:04 -0500

    Basic model
    creates a basic word set model

---
+++ Project1MagneticPoetry/Wordset.swift
@@ -0,0 +1,20 @@
+//
+//  Wordset.swift
+//  Project1MagneticPoetry
+//
+//  Created by Benjamin Wilcox on 2/28/17.
+//  Copyright © 2017 Benjamin Wilcox. All rights reserved.
+//
+
+import Foundation
+
+class WordSet {
+    
+    var name: String!
+    var words: [String]!
+    
+    init(name: String, words: [String]) {
+        self.name = name
+        self.words = words
+    }
+}
