4b04c2f8b4697fde5b8277ae5af718659c12478f
Thomas <wyzwon@gmail.com>
2017-03-26 18:54:50 -0400

    Added color to word tiles based on word type

--- Project1MagneticPoetry/WordsTableVC.swift
+++ Project1MagneticPoetry/WordsTableVC.swift
@@ -8,12 +8,26 @@
 
 import UIKit
 
+enum WordTypes: Int {
+    case Noun
+    case Verb
+    case Adjective
+    case Conjunction
+    case Pronoun
+    case Suffix
+    case Prefix
+    case Determiner
+    case Preposition
+    case Interjection
+    case Adverb
+}
+
 class WordsTableVC: UITableViewController {
     
     let wordsetList = [
-        WordSet(name: "Wolfset", words: ["wolves", "vicious", "winter", "surround", "ing", "y", "ed", "door", "help", "scent", "need", "hope", "less", "hungry", "lost", "the", "is", "am", "exhaust", "come", "run", "sleep", "darkness"]),
-        WordSet(name: "Arrrset", words: ["ahoy", "matey", "shiver", "me", "timbers", "land", "ho", "avast", "lily-livered", "sea", "scallywag","s", "raise", "the", "jolly", "Roger", "doubloons", "Arr!", "Davey", "Jones", "locker", "a", "pirate", "'s", "life", "for", "me", "yo", "ho", "Yarr!"]),
-        WordSet(name: "Baconset", words: ["the", "bacon", "most", "bacon", "important", "bacon", "meal", "bacon", "of", "bacon", "the", "bacon", "day", "bacon", "serving", "bacon", "it", "bacon", "up", "bacon", "Bacon", "'s", "bacon", "way"])
+        WordSet(name: "Wolf set", words: [("wolves",WordTypes.Noun), ("vicious",WordTypes.Adjective), ("winter",WordTypes.Noun), ("surround",WordTypes.Verb), ("ing",WordTypes.Suffix), ("y",WordTypes.Suffix), ("ed",WordTypes.Suffix), ("door",WordTypes.Noun), ("help",WordTypes.Verb), ("scent",WordTypes.Noun), ("need",WordTypes.Verb), ("hope",WordTypes.Noun), ("less",WordTypes.Pronoun), ("hungry",WordTypes.Adjective), ("lost",WordTypes.Adjective), ("the",WordTypes.Determiner), ("is",WordTypes.Verb), ("am",WordTypes.Verb), ("exhaust",WordTypes.Verb), ("come",WordTypes.Verb), ("run",WordTypes.Verb), ("sleep",WordTypes.Verb), ("darkness",WordTypes.Noun)]),
+        WordSet(name: "Arrr set", words: [("ahoy",WordTypes.Interjection), ("matey",WordTypes.Noun), ("shiver",WordTypes.Verb), ("me",WordTypes.Pronoun), ("timbers",WordTypes.Noun), ("land",WordTypes.Noun), ("ho",WordTypes.Interjection), ("avast",WordTypes.Verb), ("lily-livered",WordTypes.Adjective), ("sea",WordTypes.Noun), ("scallywag",WordTypes.Noun),("s",WordTypes.Suffix), ("raise",WordTypes.Verb), ("the",WordTypes.Determiner), ("jolly",WordTypes.Adjective), ("Roger",WordTypes.Pronoun), ("doubloons",WordTypes.Noun), ("Arr!",WordTypes.Interjection), ("Davey",WordTypes.Pronoun), ("Jones",WordTypes.Pronoun), ("locker",WordTypes.Noun), ("a",WordTypes.Determiner), ("pirate",WordTypes.Noun), ("'s",WordTypes.Suffix), ("life",WordTypes.Noun), ("for",WordTypes.Preposition), ("me",WordTypes.Pronoun), ("yo",WordTypes.Interjection), ("ho",WordTypes.Interjection), ("Yarr!",WordTypes.Interjection)]),
+        WordSet(name: "Bacon set", words: [("the",WordTypes.Determiner), ("bacon",WordTypes.Noun), ("most",WordTypes.Adjective), ("umami",WordTypes.Noun), ("important",WordTypes.Adjective), ("juicy",WordTypes.Adjective), ("meal",WordTypes.Noun), ("bacon",WordTypes.Noun), ("of",WordTypes.Preposition), ("fried",WordTypes.Adjective), ("the",WordTypes.Determiner), ("bacon",WordTypes.Noun), ("day",WordTypes.Noun), ("bacon",WordTypes.Noun), ("served",WordTypes.Verb), ("plater",WordTypes.Noun), ("ing",WordTypes.Suffix), ("spice",WordTypes.Noun), ("it",WordTypes.Pronoun), ("bacon",WordTypes.Noun), ("up",WordTypes.Adverb), ("bacon",WordTypes.Noun), ("bacon",WordTypes.Noun), ("'s",WordTypes.Suffix), ("pork",WordTypes.Noun), ("a",WordTypes.Determiner), ("on",WordTypes.Preposition), ("way",WordTypes.Noun)])
     ]
     
     var selectedWordset: WordSet!
@@ -51,11 +65,6 @@ class WordsTableVC: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
         cell.textLabel?.text = wordsetList[indexPath.row].name
-        
-        /*if (UIDevice.current.userInterfaceIdiom == .pad) {
-            cell.textLabel?.font.set
-            print("the point size is:  /(cell.textLabel?.font.pointSize)")
-        }*/
         
         return cell
     }
